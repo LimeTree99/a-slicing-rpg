21be1e7df16da998f43199ab7a1dff4991c50436
verdonh <36863148+verdonh@users.noreply.github.com>
2020-08-03 16:35:35 -0400

    background added and aron is at center of screan

---
+++ lib/sprite.py
@@ -0,0 +1,88 @@
+import pygame
+from lib.colour import *
+
+class Sprite:
+    def __init__(self, display, pos, push, friction, max_velocity):
+        self.display = display
+        self.x = pos[0]
+        self.y = pos[1]
+        self.vx = 0
+        self.vy = 0
+        self.ax = 0
+        self.ay = 0
+        self.push = push
+        self.friction = friction
+        self.max_v = max_velocity
+        self.left = False
+        self.down = False
+        self.up = False
+        self.right = False
+        self.downpace = False
+
+        self.radious = 40
+
+    def draw(self):
+        pygame.draw.circle(self.display, white, [int(self.x), int(self.y)], self.radious)
+
+    def change_values(self):
+        self.vx += self.ax
+        new_x = self.x + self.vx
+
+        self.vy += self.ay
+        new_y = self.y + self.vy
+
+        if self.vx > self.max_v:
+            self.vx = self.max_v
+        if self.vy > self.max_v:
+            self.vy = self.max_v
+        if self.vx < -self.max_v:
+            self.vx = -self.max_v
+        if self.vy < -self.max_v:
+            self.vy = -self.max_v
+
+        
+        if self.vx < self.friction and self.vx > -self.friction and \
+           (not self.left) and (not self.right):
+            self.vx = 0
+            
+        if self.vx < 0:
+            self.vx += self.friction
+        elif self.vx > 0:
+            self.vx -= self.friction
+
+        if self.vy < self.friction and self.vy > -self.friction and \
+           (not self.up) and (not self.down):
+            self.vy = 0
+        if self.vy < 0:
+            self.vy += self.friction
+        elif self.vy > 0:
+            self.vy -= self.friction
+
+        return new_x, new_y
+
+    def update(self):       
+        # these reset acceleration to 0 when a button is not pressed
+        if not (self.left and self.right):
+            self.ax = 0
+
+        if not (self.up and self.down):
+            self.ay = 0
+
+        if self.right:
+            self.ax = -self.push
+        if self.left:
+            self.ax = self.push
+        if self.down:
+            self.ay = -self.push
+        if self.up:
+            self.ay = self.push
+
+        self.x, self.y = self.change_values()
+
+        self.draw()
+
+
+
+
+
+
---
+++ lib/window.py
@@ -0,0 +1,63 @@
+import pygame
+import os
+
+class Window:
+    def __init__(self,
+                 window_name,
+                 corner_image = os.path.split(__file__)[0] + '/images/flower_corner_image.png',
+                 fps = 60):
+        self.fps = fps
+        self.end = False
+        self.background_on = True
+        self.background_colour = (0,0,0)
+        self.events = 0
+        
+        pygame.init()
+        self.display = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        
+        pygame.mouse.set_visible(False)
+        pygame.display.set_caption(window_name)
+        icon = pygame.image.load(corner_image)
+        pygame.display.set_icon(icon)
+        self.clock = pygame.time.Clock()
+
+    def update(self):
+        pass
+    
+    def event_handle(self, event):
+        pass
+
+    def keyup(self, key):
+        pass
+
+    def keydown(self, key):
+        if key == pygame.K_ESCAPE:
+            self.end = True 
+
+    def run(self):
+        while not self.end:
+            self.events = pygame.event.get()
+            for event in self.events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()  
+                    self.end = True
+                if event.type == pygame.KEYDOWN:
+                    self.keydown(event.key)
+                if event.type == pygame.KEYUP:
+                    self.keyup(event.key)
+                self.event_handle(event)
+
+            
+            self.display.fill(self.background_colour)
+            self.update()
+            pygame.display.update()
+            
+            self.clock.tick(self.fps)
+
+
+
+
+if __name__ == '__main__':
+    
+    game = Window(800, 500, 'Working Title')
+    game.run()
