0e5cb3aaa1145b403a76329dfe8cf9a6b9192455
verdonh <36863148+verdonh@users.noreply.github.com>
2020-08-04 21:04:34 -0400

    clean up number one

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,3 +2,4 @@ from .background import Background
 from .sprite import Sprite
 from .window import Window
 from .basics import Base
+from .camera import Camera
--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 import pygame, math
-from lib import color, Sprite, Window, Background, math
-
-
-class Shift_sprite(Sprite):
-    def __init__(self, display, pos, push, friction, max_velocity):
-        super().__init__(display, pos, push, friction, max_velocity)
-
-        self.onscreen_pos = pos
-        self.curr_onscreen_pos = pos
-        self.accelerate = 5
-
-    def draw(self, camera):
-        pygame.draw.circle(self.display, color.white, self.onscreen_pos, self.radious)
-
-    def to_onscreen_pos(self):
-        if self.onscreen_pos != self.curr_onscreen_pos:
-            pass
-
+from lib import color, Sprite, Window, Background, Camera
 
 
 class Alarm:
@@ -56,49 +39,6 @@ class Animate:
 
         self.display.blit(self.frames[self.frame], pos)
         
-
-    
-class Camera:
-    def __init__(self, offset=[0,0], follow_speed=1):
-        self.offset = offset
-        self.follow_speed = follow_speed
-        self.pos = [0,0]
-
-    def update(self):
-        pass
-
-    def go_to(self, pos):
-        dx = abs(self.pos[0] - pos[0])
-        dy = abs(self.pos[1] - pos[1])
-        min_x = dx/self.follow_speed
-        min_y = dy/self.follow_speed
-
-        dirx = 0
-        if (pos[0] - self.pos[0]) > min_x or (self.pos[0] - pos[0]) > min_y:
-            if pos[0] < self.pos[0]:
-                dirx = -1
-            else:
-                dirx = 1
-        diry = 0
-        if (pos[1] - self.pos[1]) > min_x or (self.pos[1] - pos[1]) > min_y:
-            if pos[1] < self.pos[1]:
-                diry = -1
-            else:
-                diry = 1
-
-        self.pos[0] += dirx * min_x
-        self.pos[1] += diry * min_y
-        
-
-    def set_pos(self, pos):
-        self.pos = pos
-
-    def get_int_pos(self):
-        return int(self.pos[0]+self.offset[0]), int(self.pos[1]+self.offset[1])
-
-    def get_pos(self):
-        return self.pos[0]+self.offset[0], self.pos[1]+self.offset[1]
-
         
 class main_w_aron(Window):
     def __init__(self):
@@ -107,7 +47,6 @@ class main_w_aron(Window):
                             -self.display.get_height()//2), 7)
 
         self.aron = Sprite(self.display, (0,0), 1, 0.5, 20)
-
         self.bg = Background(self.display)
     
     def update(self):
