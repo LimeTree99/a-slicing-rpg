dff2e360396178050da482a6b485cdf8a72302c3
verdonh <36863148+verdonh@users.noreply.github.com>
2020-08-06 20:08:09 -0400

    animate is on screen, no order has been created it just loops through everything

--- lib/background.py
+++ lib/background.py
@@ -9,8 +9,7 @@ class Background:
         pass
 
     def rect(self, color, rect):
-        pygame.draw.rect(self.display, color, 
-                        [rect[0], rect[1], rect[2], rect[3]])
+        pygame.draw.rect(self.display, color,rect)
 
     def draw(self, camera):
         self.rect((10,100, 200), (100 - camera[0],100 - camera[1],100,100))
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, math
+from pathlib import Path
 from lib import color, Sprite, Window, Background, Camera
 
 
@@ -19,26 +20,58 @@ class Alarm:
             return True
         return False
 
+
 class Animate:
-    def __init__(self, display, fr_time, frames, start_frame=0):
+    def __init__(self, display, pos, image_name, type, split_size, fr_time, folder=Path().absolute()):
         self.display = display
+        self.pos = pos
+        self.image_name = image_name
+        self.type = type
+        self.split_size = split_size
         self.fr_time = fr_time
-        self.frames = frames
-        self.frame = start_frame
-        self.start_frame = start_frame
         self.swap = Alarm(fr_time)
+        self.path = folder.joinpath("images/" + type + '/' + image_name)
+        self.screen = self.load_image(self.path)
+        self.frame_num = 0
+        self.frames = self.split_frames(self.screen, self.split_size)
+        self.group = {'all':self.frames}
+        self.state = 'all'
 
+    def get_screen(self):
+        return self.screen
 
-    def update(self, pos):
+    def load_image(self, path):
+        return pygame.image.load(str(path))
+
+    def split_frames(self, screen, size):
+        frames = []
+        for x in range(0, screen.get_width(), size[0]):
+            for y in range(0, screen.get_height(), size[1]):
+                add = screen.subsurface((x, y, size[0], size[1]))
+                add = add.copy()
+                frames.append(add)
+        return frames
+
+    def add_group(self, name, start_fr, end_fr):
+        self.group[name] = frames[start_fr : end_fr]
+
+    def next_fr(self, group):
+        if self.frame_num >= len(self.frames)-1:
+            self.frame_num = 0
+        else:
+            self.frame_num += 1
+        return self.frames[self.frame_num]
+
+    def current_frame(self, group):
+        return self.frames[self.frame_num]
+
+    def update(self):
         if self.swap.timer():
-            # go to next frame
-            if self.frame >= len(self.frames)-1:
-                self.frame = 0
-            else:
-                self.frame += 1
+            frame = self.next_fr(self.state)
 
-        self.display.blit(self.frames[self.frame], pos)
-        
+    def draw(self, pos):
+        self.display.blit(self.current_frame(self.state), (self.pos[0] - pos[0], self.pos[1] - pos[1]))
+      
         
 class main_w_aron(Window):
     def __init__(self):
@@ -48,15 +81,23 @@ class main_w_aron(Window):
 
         self.aron = Sprite(self.display, (0,0), 1, 0.5, 20)
         self.bg = Background(self.display)
-    
+
+        self.animate = Animate(self.display, (10, 50), "character.png", "sprite", (64, 64), 1000)
+
+
+
     def update(self):
         self.bg.update()
         self.aron.update()
+        self.animate.update()
+
         self.camera.go_to(self.aron.pos)
 
     def draw(self):
+        
         self.bg.draw(self.camera.get_pos())
         self.aron.draw(self.camera.get_pos())
+        self.animate.draw(self.camera.get_pos())
 
     def keydown(self, key):
         super().keydown(key)
@@ -86,6 +127,7 @@ class main(main_w_aron):
 
 
 if __name__ == "__main__":
+    print('----START----')
     main().run()
     print('----END----')
 
